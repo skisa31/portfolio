d3d25203b047792137eb55f6e6122f0a03ec41e2
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-15 19:05:26 +0900

    Linkのエラーを解消

--- blog/app/Header.tsx
+++ blog/app/Header.tsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <header className="sticky top-0 border-b z-10 bg-white">
       <div className="max-w-4xl mx-auto flex justigy-between items-center h-12">
-        <Link>
-          <a>Logo</a>
+        <Link href="/blog">
+          Logo
         </Link>
         <div>Link</div>
       </div>
