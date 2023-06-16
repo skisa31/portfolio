515bb0386a183114d347964a8a91d481edf2b721
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-16 15:32:23 +0900

    画像の表示に伴い改装

--- blog/app/components/PostCard.tsx
+++ blog/app/components/PostCard.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const PostCard = ({ post }) => {
   return (
     <Link href={`/post/${post.slug}`}>
-      {post.frontMatter.title}
+      <div className="border rounded-lg">
+        <Image
+          src={`/${post.frontMatter.image}`}
+          width={600}
+          height={400}
+          alt={post.frontMatter.title}
+        />
+      </div>
+      <div className="px-2 py-4">
+        <h1 className="font-bolld text-lg">{post.frontMatter.title}</h1>
+        <span>{post.frontMatter.date}</span>
+      </div>
     </Link>
   );
 };
