ef5f18c0728cadee5a15ae6debcb08f30b932d7a
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-16 14:24:26 +0900

    matterでMarkdownのメタ情報と記事内容をそれぞれ取得

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,12 +1,14 @@
 import fs from 'fs';
-import { get } from 'http';
+import matter from 'gray-matter';
 
 const getData = async () => {
   const files: string[] = fs.readdirSync('app/posts');
   const posts = files.map((filename: string) => {
-    const slog: string = filename.replace(/\.md$/, '');
-    const fileContent = fs.readFileSync(`app/posts/${filename}`, 'utf-8');
-    console.log('slug:', slog);
+    const slug: string = filename.replace(/\.md$/, '');
+    const fileContent: string = fs.readFileSync(`app/posts/${filename}`, 'utf-8');
+    const { data, content } = matter(fileContent);
+    console.log('data:', data);
+    console.log('content:', content);
   });
   return {
     posts: [],
