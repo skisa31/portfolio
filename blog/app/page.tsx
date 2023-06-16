55f2c9c13a6150f6925f1ebf55e54125fbc31fc4
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-16 14:33:35 +0900

    matterで取得したデータとslugを持つオブジェクトを配列としてコンソールに表示

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -6,17 +6,21 @@ const getData = async () => {
   const posts = files.map((filename: string) => {
     const slug: string = filename.replace(/\.md$/, '');
     const fileContent: string = fs.readFileSync(`app/posts/${filename}`, 'utf-8');
-    const { data, content } = matter(fileContent);
-    console.log('data:', data);
-    console.log('content:', content);
+    const { data } = matter(fileContent);
+    return {
+      frontMatter: data,
+      slug,
+    };
   });
+
   return {
-    posts: [],
+    posts
   };
 };
 
-export default async function Home({ posts }) {
-  await getData();
+export default async function Home() {
+  const { posts } = await getData();
+  console.log(posts);
 
   return (
     <div className='my-8'>
