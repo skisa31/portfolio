82672e1e60d3d6d191af38e9c7ac18270bb35265
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-17 11:37:39 +0900

    layoutのテンプレを更新

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,11 +3,34 @@ import Header from './header';
 import Footer from './footer';
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const siteName = 'Kisa\'s Portfolio';
+const description = 'Kisaのポートフォリオサイトです．';
+const url = 'https://kisa-ss';
 
 export const metadata = {
-  title: 'Kisa\'s portfolio',
-  description: 'Kisa\'s portfolio',
+  title: {
+    default: siteName,
+    template: `%s - ${siteName}`,
+  },
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    local: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description,
+    site: '@shk4346',
+    creator: '@shk4346',
+  },
+  alternates: {
+    canonical: url,
+  },
 }
 
 export default function RootLayout({
@@ -16,11 +39,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="jp">
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+    <html lang="ja">
+      <body suppressHydrationWarning={true}>
+        <div className='flex flex-col min-h-screen'>
+          <Header />
+          <main className='flex-1 w-full'>
+            {children}
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   )
