867e6837a8bb9dc0deebcf49afdf2f58ff55e83a
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-17 09:45:02 +0900

    画像の表示の大きさを揃えて中央そろえに

--- blog/app/components/PostCard.tsx
+++ blog/app/components/PostCard.tsx
@@ -4,12 +4,13 @@ import Link from "next/link";
 const PostCard = ({ post }) => {
   return (
     <Link href={`/post/${post.slug}`}>
-      <div className="border rounded-lg">
+      <div className="border rounded-lg p-3 flex justify-center">
         <Image
           src={`/${post.frontMatter.image}`}
           width={600}
           height={400}
           alt={post.frontMatter.title}
+          className="object-contain m-auto h-36"
         />
       </div>
       <div className="px-2 py-4">
