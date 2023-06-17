8ba9441bb5b333867c6e3353b3c291484b4c0ec6
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-17 21:23:35 +0900

    ヘッダーの雛形

--- frontend/app/header.tsx
+++ frontend/app/header.tsx
@@ -3,12 +3,24 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header className='sticky top-0 border-b z-10 bg-white'>
-      <div className='max-w-4xl mx-auto flex justify-between items-center h-12'>
-        <Link href="/">
-          LOGO
-        </Link>
-        <div>
-          Link
+      <div className='max-w-6xl mx-auto flex justify-between items-center h-12 roboto.variable'>
+        <div className='h-max text-2xl hover:cursor-pointer'>
+          <Link href="/">
+            Kisa&apos;s Portfolio
+          </Link>
+        </div>
+        <span className='flex-1'></span>
+        <div className='text-xl px-4 py-2 hove:cursor-pointer hover:bg-gray-200'>
+          About
+        </div>
+        <div className='text-xl px-4 py-2 hove:cursor-pointer hover:bg-gray-200'>
+          Article
+        </div>
+        <div className='text-xl px-4 py-2 hove:cursor-pointer hover:bg-gray-200'>
+          Blog
+        </div>
+        <div className='text-xl px-4 py-2 hove:cursor-pointer hover:bg-gray-200'>
+          Contact
         </div>
       </div>
     </header>
