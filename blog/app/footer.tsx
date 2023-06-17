ea4ebc4ce377236b01a6f16d3976589bbb9f167b
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-17 22:51:38 +0900

    ポートフォリオサイトと共通化

--- blog/app/footer.tsx
+++ blog/app/footer.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+
 const Footer = () => {
   return (
     <footer className="bg-gray-100">
-      <div className="max-w-4xl w-full mx-auto h-24  flex items-center justify-center">
-        <div>
-          © {new Date().getFullYear()} Kisa&apos;s blog
+      <div className="max-w-6xl w-full mx-auto h-24 flex">
+        <div className="pt-4">
+          © {new Date().getFullYear()} Kisa
+        </div>
+        <span className="flex-1"></span>
+        <div className="pt-4 px-2">
+          <a href="https://twitter.com/shk4346" target="_brank" rel="noopener noreferrer">
+            <AiFillTwitterCircle size={24} />
+          </a>
+        </div>
+        <div className="pt-4 px-2">
+          <a href="https://github.com/skisa31" target="_brank" rel="noopener noreferrer">
+            <AiFillGithub size={24} />
+          </a>
         </div>
       </div>
     </footer>
--- blog/app/header.tsx
+++ blog/app/header.tsx
@@ -3,12 +3,24 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header className='sticky top-0 border-b z-10 bg-white'>
-      <div className='max-w-4xl mx-auto flex justify-between items-center h-12'>
-        <Link href="/">
-          LOGO
-        </Link>
-        <div>
-          Link
+      <div className='max-w-6xl mx-auto flex justify-between items-center h-12 roboto.variable'>
+        <div className='h-max text-2xl hover:cursor-pointer'>
+          <Link href="/">
+            Kisa&apos;s Blog
+          </Link>
+        </div>
+        <span className='flex-1'></span>
+        <div className='text-xl px-4 py-2 transition duration-300 hover:scale-110'>
+          About
+        </div>
+        <div className='text-xl px-4 py-2 transition duration-300 hover:scale-110'>
+          Article
+        </div>
+        <div className='text-xl px-4 py-2 transition duration-300 hover:scale-110'>
+          <Link href="http://localhost:3031">Blog</Link>
+        </div>
+        <div className='text-xl px-4 py-2 transition duration-300 hover:scale-110'>
+          Contact
         </div>
       </div>
     </header>
