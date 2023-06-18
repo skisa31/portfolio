1938d24ae236c1e4d730d9e1da5659b048f61b58
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-18 11:35:22 +0900

    サイズを調整

--- frontend/app/header.tsx
+++ frontend/app/header.tsx
@@ -1,27 +1,28 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className='sticky top-0 border-b z-10 bg-white'>
-      <div className='max-w-6xl mx-auto flex justify-between items-center h-12 roboto.variable'>
+    <header className='sticky top-0 h-16 border-b z-10 bg-white'>
+      <div className='max-w-6xl mx-auto pt-3 flex justify-between items-center h-12 roboto.variable'>
         <div className='h-max text-2xl hover:cursor-pointer'>
           <Link href="/">
-            Kisa&apos;s Portfolio
+            <Image src='/logo.svg' width={50} height={50} />
           </Link>
         </div>
         <span className='flex-1'></span>
         <div>
-          <ul className='flex text-xl'>
-            <li className='px-4 py-2 transition duration-300 hover:scale-110'>
+          <ul className='flex text-xl my-auto'>
+            <li className='px-4 py-2 transition duration-300 hover:scale-125'>
               About
             </li>
-            <li className='px-4 py-2 transition duration-300 hover:scale-110'>
+            <li className='px-4 py-2 transition duration-300 hover:scale-125'>
               Article
             </li>
-            <li className='px-4 py-2 transition duration-300 hover:scale-110'>
+            <li className='px-4 py-2 transition duration-300 hover:scale-125'>
               <Link href="http://localhost:3031">Blog</Link>
             </li>
-            <li className='px-4 py-2 transition duration-300 hover:scale-110'>
+            <li className='px-4 py-2 transition duration-300 hover:scale-125'>
               <Link href='/contact'> Contact </Link>
             </li>
           </ul>
