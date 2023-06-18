064c545f9b31f133ea3bd9ca3671b4e94f08308b
Shoki Sakai <sshk1031.ski+git@gmail.com>
2023-06-18 14:11:48 +0900

    instagramのアイコンをフォームの雛形の作成

--- frontend/app/contact/page.tsx
+++ frontend/app/contact/page.tsx
@@ -2,8 +2,10 @@
 
 import Header from "../header";
 import Footer from "../footer";
+import InputFiled from "../components/InputField";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -66,12 +68,65 @@ const Contact = () => {
               github/skisa31
             </a>
           </div>
+          <div className="col-span-1 flex">
+            <div className="pr-4">
+              <AiFillInstagram size={32} />
+            </div>
+            <a href="https://www.instagram.com/s.ski_31/" target="_brank" rel="noopener noreferrer" className="flex-1 text-xl text-left hover:underline hover:cursor-pointer">
+              s.ski_31
+            </a>
+          </div>
         </div>
         <div className='text-2xl mx-auto pt-10 text-center'>
           <p className="font-notoSansJp">お問い合わせ</p>
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="pt-4">
+                <label htmlFor="name" className="block mb-2 text-xl text-left font-medium text-gray-900 font-notoSansJp dark:text-white">
+                  お名前
+                  <sup className="text-red-500"> *</sup>
+                </label>
+                <input type="text" id="name" {...register('name')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="お名前" required />
+                {errors.name && <p className="text-sm text-left text-red-500">{errors.message?.message}</p>}
+              </div>
+            </form>
+          </div>
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="pt-4">
+                <label htmlFor="email" className="block mb-2 text-xl text-left font-medium text-gray-900 font-notoSansJp dark:text-white">
+                  メールアドレス
+                  <sup className="text-red-500"> *</sup>
+                </label>
+                <input type="text" id="email" {...register('email')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="メールアドレス" required />
+                {errors.name && <p className="text-sm text-left text-red-500">{errors.message?.message}</p>}
+              </div>
+            </form>
+          </div>
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="pt-4">
+                <label htmlFor="massage" className="block mb-2 text-xl text-left font-medium text-gray-900 font-notoSansJp dark:text-white">
+                  お問い合わせ内容
+                  <sup className="text-red-500"> *</sup>
+                </label>
+                <textarea id="message" {...register('message')} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="お問い合わせ内容" required />
+                {errors.name && <p className="text-sm text-left text-red-500">{errors.message?.message}</p>}
+              </div>
+            </form>
+          </div>
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="pt-4">
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  送信
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   )
 }
 
